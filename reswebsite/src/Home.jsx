ee2a95e9556f8ae45057e312bd1d1c3f6f62478b
Bhavik Jain <bhvaikjain@gmail.com>
2021-07-13 19:49:59 +0530

    update page

--- reswebsite/src/Home.jsx
+++ reswebsite/src/Home.jsx
@@ -4,11 +4,11 @@ import React  from "react";
 const Home =() =>{
     return(
       <div>
-        <section className="" id="header">
+        <section className="d-flex align-items-center" id="header">
           <div className="container-fluid nav_bg">
             <div className="row">
               <div className="col-10 mx-auto">
-                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
                   <h1>
                     Grow Your Business With <strong className="brand-name">Alpha Creation</strong>
                   </h1>
@@ -16,7 +16,7 @@ const Home =() =>{
                     We are the team of smart developers 
                   </h2>
                   <div className="mt-3">
-                    <a href="" className="btn-get-started "> 
+                    <a href="" className="btn btn-outline-info"> 
                     Get Started 
                     </a>
                   </div>
