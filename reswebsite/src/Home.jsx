63926ecc0d3f04133a9677ec230d16cc0c90675c
Bhavik Jain <bhvaikjain@gmail.com>
2021-07-13 19:27:40 +0530

    Home Page

---
+++ reswebsite/src/Home.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React  from "react";
+
+const Home =() =>{
+    return(
+      <div>
+        <section className="" id="header">
+          <div className="container-fluid nav_bg">
+            <div className="row">
+              <div className="col-10 mx-auto">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+                  <h1>
+                    Grow Your Business With <strong className="brand-name">Alpha Creation</strong>
+                  </h1>
+                  <h2 className="my-3">
+                    We are the team of smart developers 
+                  </h2>
+                  <div className="mt-3">
+                    <a href="" className="btn-get-started "> 
+                    Get Started 
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  };
+  
+  export default Home;
